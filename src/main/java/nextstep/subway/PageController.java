9c1a69ce42125004db9203d445db3600f0aecbc9
sk1737030 <sk1737030@naver.com>
2021-04-15 17:07:53 +0900

    #5 active profile에 따른 분리 Logger Logging

--- src/main/java/nextstep/subway/PageController.java
+++ src/main/java/nextstep/subway/PageController.java
@@ -1,11 +1,17 @@
 package nextstep.subway;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.http.MediaType;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class PageController {
+
+    private static final Logger log = LoggerFactory.getLogger(PageController.class);
+    private static final Logger fileLogger = LoggerFactory.getLogger("file");
+
     @GetMapping(value = {
             "/",
             "/stations",
@@ -18,6 +24,9 @@ public class PageController {
             "/mypage/edit",
             "/favorites"}, produces = MediaType.TEXT_HTML_VALUE)
     public String index() {
+        log.error("Logger Test An ERROR Message ");
+        fileLogger.error("파일 로깅 테스트 입니다.");
+
         return "index";
     }
 }
