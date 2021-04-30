ceca1c26087bd51b730ee5b94bb9f7b973237185
sk1737030 <sk1737030@naver.com>
2021-04-30 20:06:23 +0900

    ADD: 페이징 쿼리 추가 #10

--- src/main/java/nextstep/subway/favorite/domain/FavoriteRepository.java
+++ src/main/java/nextstep/subway/favorite/domain/FavoriteRepository.java
@@ -1,9 +1,15 @@
 package nextstep.subway.favorite.domain;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import java.util.List;
 
 public interface FavoriteRepository extends JpaRepository<Favorite, Long> {
     List<Favorite> findByMemberId(Long memberId);
+
+    @Query(value = "SELECT * FROM favorite WHERE favorite.member_id = ?1", nativeQuery = true)
+    Page<Favorite> findAllByMemberId(Long memberId, Pageable pageable);
 }
